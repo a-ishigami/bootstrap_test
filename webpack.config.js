8329ea592da8989d44b4f765936941b4ec771ee9
Atsushi Ishigami <sapijwjw5959@gmail.com>
2018-07-29 19:57:42 +0900

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'production',
+  mode: 'development',
+
+  // ローカル開発用環境を立ち上げる
+  // 実行時にブラウザが自動的に localhost を開く
+  devServer: {
+    contentBase: 'dist',
+    open: true
+  },
+
   module: {
     rules: [
       {
